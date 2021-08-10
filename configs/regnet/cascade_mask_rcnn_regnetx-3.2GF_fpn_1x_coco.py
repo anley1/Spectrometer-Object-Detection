083573bda5f230a3b02accb098910bcdd75a8acd
Alexander Leye <anley1@student.monash.edu>
2021-08-10 23:31:14 +1000

    Add HTC and backbone testing configs

---
+++ configs/regnet/cascade_mask_rcnn_regnetx-3.2GF_fpn_1x_coco.py
@@ -0,0 +1,21 @@
+_base_ = [
+    '../bead/cascade_mask_rcnn_r50_fpn_1x_coco.py',
+]
+model = dict(
+    pretrained='open-mmlab://regnetx_3.2gf',
+    backbone=dict(
+        _delete_=True,
+        type='RegNet',
+        arch='regnetx_3.2gf',
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch'),
+    neck=dict(
+        type='FPN',
+        in_channels=[96, 192, 432, 1008],
+        out_channels=256,
+        num_outs=5))
+
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.00005)
--- configs/res2net/cascade_mask_rcnn_r2_101_fpn_20e_coco.py
+++ configs/res2net/cascade_mask_rcnn_r2_101_fpn_20e_coco.py
@@ -1,4 +1,8 @@
-_base_ = '../cascade_rcnn/cascade_mask_rcnn_r50_fpn_20e_coco.py'
+#_base_ = '../cascade_rcnn/cascade_mask_rcnn_r50_fpn_20e_coco.py'
+_base_ = '../bead/cascade_mask_rcnn_r50_fpn_1x_coco.py'
 model = dict(
     pretrained='open-mmlab://res2net101_v1d_26w_4s',
     backbone=dict(type='Res2Net', depth=101, scales=4, base_width=26))
+
+optimizer_config = dict(_delete_=True,
+                        grad_clip=dict(max_norm=35.0, norm_type=2))
