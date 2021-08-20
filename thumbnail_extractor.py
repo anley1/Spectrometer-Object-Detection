558e2f3410a834eb8cba6d90a977c9d0bbef0c54
Alexander Leye <anley1@student.monash.edu>
2021-08-20 13:22:36 +1000

    Allow failed frame read in thumbnail extractor

--- thumbnail_extractor.py
+++ thumbnail_extractor.py
@@ -6,20 +6,22 @@ def run_example():
   """Extract frames from the video and creates thumbnails for one of each"""
   # Extract frames from video
   print("Extract frames from video")
-  frames = video_to_frames('/data/video/')
+  video_name = "GH010016.MP4"
+  frames = video_to_frames('/home/anley1/aa20/Dataset/Videos/%s' % video_name)
 
   # Generate and save thumbs
   print("Generate and save thumbs")
   for i in range(len(frames)):
     thumb = image_to_thumbs(frames[i])
-    os.makedirs('frames/%d' % i)
+    os.makedirs('frames/%s/%d' % (video_name, i))
     for k, v in thumb.items():
-      cv2.imwrite('frames/%d/%s.png' % (i, k), v)
+      cv2.imwrite('frames/%s/%d/%s.png' % (video_name, i, k), v)
 
 def video_to_frames(video_filename):
     """Extract frames from video"""
     cap = cv2.VideoCapture(video_filename)
     video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
+    print(video_length)
     frames = []
     if cap.isOpened() and video_length > 0:
         frame_ids = [0]
@@ -29,12 +31,15 @@ def video_to_frames(video_filename):
                          round(video_length * 0.5),
                          round(video_length * 0.75),
                          video_length - 1]
+        print(frame_ids)
         count = 0
         success, image = cap.read()
-        while success:
-            if count in frame_ids:
+        while success or count < video_length:
+            if count in frame_ids and success:
                 frames.append(image)
             success, image = cap.read()
+            print(count)
+            print(success)
             count += 1
     return frames
 
