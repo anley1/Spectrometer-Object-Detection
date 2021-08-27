2e1439945c4eeee7cce9e15e072be9119b84c017
Alexander Leye <anley1@student.monash.edu>
2021-08-27 19:16:35 +1000

    Norm scaling and 28 epoch HTC

--- configs/hrnet/htc_hrnetv2p_w40_28e_coco.py
+++ configs/hrnet/htc_hrnetv2p_w40_28e_coco.py
@@ -2,3 +2,8 @@ _base_ = './htc_hrnetv2p_w40_20e_coco.py'
 # learning policy
 lr_config = dict(step=[24, 27])
 runner = dict(type='EpochBasedRunner', max_epochs=28)
+
+# optimizer_config = dict(_delete_=True)
+optimizer_config = dict(_delete_=True,
+                        grad_clip=dict(max_norm=35.0, norm_type=2))
+checkpoint_config = dict(max_keep_ckpts=1)
